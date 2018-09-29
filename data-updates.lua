7a40cf495da897db5c63d98d19394f0934022543
Nicholas Dunn <nexela@gmail.com>
2018-09-29 18:40:20 -0400

    stdlib fix

--- data-updates.lua
+++ data-updates.lua
@@ -1,3 +1,3 @@
-require('stdlib/data/data').create_data_globals()
+require("__stdlib__/stdlib/data/data").create_data_globals()
 
 local default = setmetatable({}, require("__stdlib__/stdlib/utils/classes/string_array"))
